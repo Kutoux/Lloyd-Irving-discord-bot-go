d1e3ba5df1840fdd240e83056740fae70d1e3895
Kutoux <bobby.king@umbc.edu>
2023-04-29 18:54:07 -0400

    Moved vow selection into a handler

--- main.go
+++ main.go
@@ -40,36 +40,7 @@ func main() {
 		}
 
 		if args[0] == vow {
-			dwarvenVows := []string{
-				"Dwarven Vow #1: Let's all work together for a peaceful world.",
-				"Dwarven Vow #2: Never abandon someone in need.",
-				"Dwarven Vow #4: Don't depend on others. Walk on your own two legs.",
-				"Dwarven Vow #5: The more you add, the worse it gets.",
-				"Dwarven Vow #7: Justice and love will always win.",
-				"Dwarven Vow #7: Goodness and love will always win.",
-				"Dwarven Vow #9: Fall down seven times, stand up eight.",
-				"Dwarven Vow #10: Play hard, play often.",
-				"Dwarven Vow #11: Lying is the first step down the path of thievery.",
-				"Dwarven Vow #14: Even a small star shines in the darkness.",
-				"Dwarven Vow #16: You can do anything if you try.",
-				"Dwarven Vow #18: It's better to be deceived than to deceive.",
-				"Dwarven Vow #24: Never let your feet run faster than your shoes.",
-				"Dwarven Vow #32: Cross even a stone bridge after you've tested it.",
-				"Dwarven Vow #41: It's better to begin in the evening than not at all.",
-				"Dwarven Vow #41: Haste makes waste.",
-				"Dwarven Vow #43: Never forget the basics.",
-				"Dwarven Vow #55: A bad workman blames his tools.",
-				"Dwarven Vow #108: Let sleeping dogs lie.",
-				"Dwarven Vow #134: Compassion benefits all men.",
-			}
-
-			selection := rand.Intn(len(dwarvenVows))
-
-			embed := discordgo.MessageEmbed{
-				Title: dwarvenVows[selection],
-			}
-
-			s.ChannelMessageSendEmbed(m.ChannelID, &embed)
+			DwarvenVowHandler(s, m)
 		}
 	})
 
@@ -87,4 +58,37 @@ func main() {
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
 	<-sc
+}
+
+func DwarvenVowHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
+	dwarvenVows := []string{
+		"Dwarven Vow #1: Let's all work together for a peaceful world.",
+		"Dwarven Vow #2: Never abandon someone in need.",
+		"Dwarven Vow #4: Don't depend on others. Walk on your own two legs.",
+		"Dwarven Vow #5: The more you add, the worse it gets.",
+		"Dwarven Vow #7: Justice and love will always win.",
+		"Dwarven Vow #7: Goodness and love will always win.",
+		"Dwarven Vow #9: Fall down seven times, stand up eight.",
+		"Dwarven Vow #10: Play hard, play often.",
+		"Dwarven Vow #11: Lying is the first step down the path of thievery.",
+		"Dwarven Vow #14: Even a small star shines in the darkness.",
+		"Dwarven Vow #16: You can do anything if you try.",
+		"Dwarven Vow #18: It's better to be deceived than to deceive.",
+		"Dwarven Vow #24: Never let your feet run faster than your shoes.",
+		"Dwarven Vow #32: Cross even a stone bridge after you've tested it.",
+		"Dwarven Vow #41: It's better to begin in the evening than not at all.",
+		"Dwarven Vow #41: Haste makes waste.",
+		"Dwarven Vow #43: Never forget the basics.",
+		"Dwarven Vow #55: A bad workman blames his tools.",
+		"Dwarven Vow #108: Let sleeping dogs lie.",
+		"Dwarven Vow #134: Compassion benefits all men.",
+	}
+
+	selection := rand.Intn(len(dwarvenVows))
+
+	embed := discordgo.MessageEmbed{
+		Title: dwarvenVows[selection],
+	}
+
+	s.ChannelMessageSendEmbed(m.ChannelID, &embed)
 }
